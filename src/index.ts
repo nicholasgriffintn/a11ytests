d850e9b0143c67b5e23f817b75d8bd17c55ef4b3
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-04-11 14:53:26 +0100

    chore: removing log

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const visitCounts = new Map<string, number>();
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
-		console.log(`${url.pathname} ${request.headers.get('x-frame-options')}`)
+		
 		switch (url.pathname) {
 			case '/status':
 				return new Response('OK');
